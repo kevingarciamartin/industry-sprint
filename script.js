a0a9f341df3b8902eb9caaf986d1a21c0955e4b7
Kevin Garcia Martin <kevingm@live.se>
2024-10-22 14:54:32 +0200

    Change header data when clicking mobile menu button

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  header.dataset.mobileMenuOpen =
+    header.dataset.mobileMenuOpen === "false" ? "true" : "false";
+  menuBtn.disabled = true;
+  setTimeout(() => {
+    menuBtn.disabled = false;
+  }, 50);
+});
